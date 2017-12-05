7e4fd2e1bbfe7c3ff21a97dd9e8ffd70be160be3
bs <brian.snedic@gmail.com>
2017-12-04 21:40:26 -0700

    nvidia model

---
+++ project3/model.py
@@ -0,0 +1,58 @@
+# Import the data
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if not reader.line_num == 1:
+            lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+
+
+
+# Train a model
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers import pooling, MaxPooling2D
+
+
+#https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/
+model = Sequential()
+
+#input
+model.add(Lambda(lambda x: (x / 255.0) -0.5, input_shape=(160,320,3)))
+
+#normalization
+model.add(Convolution2D(24, 5, 5, activation="relu"))
+model.add(Convolution2D(36, 5, 5, activation="relu"))
+model.add(Convolution2D(48, 5, 5, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
