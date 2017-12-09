8d654d5579402304a0d665775ca906941be73511
bs <brian.snedic@gmail.com>
2017-12-08 23:37:36 -0700

    updated model strides

--- project3/model.py
+++ project3/model.py
@@ -74,9 +74,9 @@ model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*3,
-                    validation_data=valid_generator, nb_val_samples=len(valid_samples)*3,
-                    nb_epoch=20)
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*3*2,
+                    validation_data=valid_generator, nb_val_samples=len(valid_samples)*3*2,
+                    nb_epoch=5)
 
 model.save('model.h5', overwrite=True)
 
