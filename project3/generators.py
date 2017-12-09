1686a38878369f67fee4d09345d097eafda045bd
Brian Snedic <brian.snedic@gmail.com>
2017-12-09 00:38:30 +0000

    code updates

--- project3/generators.py
+++ project3/generators.py
@@ -2,13 +2,14 @@ from sklearn.utils import shuffle
 from cv2 import imread, resize
 from numpy import array
 
-def batchGenerator(samples, batchSize=32, resizeRatio=(1., 1.), imgPath='./data/IMG/'):
+def batchGenerator(samples, batchSize=33, resizeRatio=(1., 1.), imgPath='./data/IMG/'):
     ''' Generate a batch of size batchSize for the sample reference provided
         input:  samples - sample list containing a fileName at index 0 and center angle at index 3
                 batchSize - the size of the desired batch, default is 32
                 resizeRatio - resize the image by this ratio, default is (1.,1.) meaning no change
                 imgPath - the directory path to the folder containing the images, default is ./data/IMG'''
     nSamples = len(samples)
+    batchSize = int(batchSize/3)
     while 1:
         shuffle(samples)
         for offset in range(0, nSamples, batchSize):
@@ -21,12 +22,12 @@ def batchGenerator(samples, batchSize=32, resizeRatio=(1., 1.), imgPath='./data/
                 centerImg = imread(name)
                 centerAngle = float(batchSample[3])
 
-                name = imgPath+batchSample[1].split('/')[-1]
-                leftImg = imread(name)
+                leftName = imgPath+batchSample[1].split('/')[-1]
+                leftImg = imread(leftName)
                 leftAngle = float(batchSample[3])
 
-                name = imgPath+batchSample[2].split('/')[-1]
-                rightImg = imread(name)
+                rightName = imgPath+batchSample[2].split('/')[-1]
+                rightImg = imread(rightName)
                 rightAngle = float(batchSample[3])
 
                 # crop image
--- project3/model.py
+++ project3/model.py
@@ -17,14 +17,14 @@ with open('./trainData/driving_log.csv') as csvfile:
         if not reader.line_num == 1:
             samples.append(line)
 train_samples, valid_samples = train_test_split(samples, test_size=0.2)
-resizeImgRatio = (0.5, 0.5)
+#resizeImgRatio = (0.5, 0.5)
 train_generator = batchGenerator(train_samples,
-                                 batchSize=32,
-                                 resizeRatio=resizeImgRatio,
+                                 batchSize=33,
+                                 #resizeRatio=resizeImgRatio,
                                  imgPath='./trainData/IMG/')
 valid_generator = batchGenerator(valid_samples,
-                                 batchSize=32,
-                                 resizeRatio=resizeImgRatio,
+                                 batchSize=33,
+                                 #resizeRatio=resizeImgRatio,
                                  imgPath='./trainData/IMG/')
 
 # Train a model
@@ -37,6 +37,14 @@ model.add(Cropping2D(cropping=((60, 22), (0, 0)), input_shape=(row, col, ch)))
 
 #normalization
 model.add(Lambda(lambda x: (x / 127.5) - 1.))#, input_shape=(row, col, ch)))
+#model.add(Lambda(lambda x: (x / 255.0) -0.5))#, input_shape=(160,320,3)))
+#
+#model.add(Convolution2D(6,5,5,activation="relu"))
+#model.add(MaxPooling2D())
+#model.add(Flatten())
+#model.add(Dense(120))
+#model.add(Dense(84))
+#model.add(Dense(1))
 
 #rest of the model
 model.add(Convolution2D(6, 5, 5, activation="relu"))
@@ -58,8 +66,8 @@ model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
-                    validation_data=valid_generator, nb_val_samples=len(valid_samples),
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*3,
+                    validation_data=valid_generator, nb_val_samples=len(valid_samples)*3,
                     nb_epoch=30)
 
 model.save('model.h5', overwrite=True)
